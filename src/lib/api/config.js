c1ec57ca1be30a72b87c7169a2880ea8de5d91be
Moniruzzaman - SDE-2 <moniruzzaman.snaptec@gmail.com>
2026-01-26 14:06:29 +0600

    update env

--- src/lib/api/config.js
+++ src/lib/api/config.js
@@ -1,4 +1,4 @@
 export const API_CONFIG = {
-  BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost/snapcart",
+  BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL || "https://rsm.com.bd",
   PREFIX: "/api/v1",
 };
