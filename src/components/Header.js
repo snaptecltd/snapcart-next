0c7e5444820527ac698d989c1ba1221a14e2edf0
Moniruzzaman - SDE-2 <moniruzzaman.snaptec@gmail.com>
2026-01-28 16:31:52 +0600

    implementd product compare page

--- src/components/Header.js
+++ src/components/Header.js
@@ -340,7 +340,7 @@ export default function Header() {
                 </Link>
 
                 <Link
-                  href="#"
+                  href="/compare"
                   className={`d-none d-md-flex text-white text-decoration-none d-flex align-items-center gap-1 ${styles.nav_link}`}
                 >
                   <i className="fas fa-code-compare"></i>
