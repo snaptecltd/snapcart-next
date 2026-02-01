1f8c6e6fb90b8d4c98138bde98cef57c88782211
Moniruzzaman - SDE-2 <moniruzzaman.snaptec@gmail.com>
2026-02-01 14:29:41 +0600

    fixed carousel

---
+++ src/components/BootstrapClient.js
@@ -0,0 +1,9 @@
+"use client";
+
+import { useEffect } from "react";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+
+export default function BootstrapClient() {
+  useEffect(() => {}, []);
+  return null;
+}
