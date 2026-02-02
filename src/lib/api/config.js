e9b5999136bde80d3c9937a62a1a2e6894245ed3
Moniruzzaman - SDE-2 <moniruzzaman.snaptec@gmail.com>
2026-02-02 20:17:09 +0600

    update config

--- src/lib/api/config.js
+++ src/lib/api/config.js
@@ -1,4 +1,4 @@
 export const API_CONFIG = {
-  BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL || "https://rsm.com.bd",
+  BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL || "https://admin.rsm.com.bd",
   PREFIX: "/api/v1",
 };
