66432dda832a6b4921a58e2b4cc4f163491c857f
Moniruzzaman - SDE-2 <moniruzzaman.snaptec@gmail.com>
2026-02-14 13:02:23 +0600

    udpate loader

--- src/app/loading.js
+++ src/app/loading.js
@@ -2,7 +2,7 @@ export default function Loading() {
   return (
     <div className="global-loader">
       <div className="loader-content">
-        <div className="logo">Snapcart</div>
+        {/* <div className="logo">Snapcart</div> */}
         <div className="spinner" />
       </div>
     </div>
